f642fd6379d20206703ece666e026d959e818c63
miniral199 <miniral199@gmail.com>
2026-02-03 11:57:22 +0300

    что то добавил что то поменял пока не идеально

--- desk.py
+++ desk.py
@@ -6,7 +6,7 @@ class Desk:
     self.y=size_y
     # количество клеток
     self.cells=size_x*size_y
-    self.enemy_field=[[""]*size_x]*size_y
+    self.enemy_field=[[""]*size_x for _ in range(size_y)]
     # self.enemy_field=[
     #   ['','',''],['','','']
     # ]
@@ -15,9 +15,21 @@ class Desk:
   def create_enemy(self):
     for y,value in enumerate(self.enemy_field):
       for x,value_x in enumerate(value):
+        # 0 - means there no ship
+        # 1 - means there ship
         self.enemy_field[y][x]=random.randint(0,1)
+
 
 
   def print_enemy(self):
     for a,b in enumerate(self.enemy_field):
-      print(a,b)
+      print(b)
+    # print(self.enemy_field[0][1]) #x=0 y=1
+  
+  # checks if there any ships?
+  def check_ships(self):
+    for i in self.enemy_field:
+      if 1 in i: 
+        return True
+    return False
+  
--- main.py
+++ main.py
@@ -1,9 +1,25 @@
 # version 1.0
 from desk import Desk
-from ship import Ships
+# from ship import Ships
 def introduction():
   print("Я делаю морской бой!")
+  print("На данный момент это игра в которой невозможно проиграть! АХАХ")
 
+def shoot(desk):
+  print("Type coordinates to shoot!")
+  try:
+    x=int(input("Type x:"))
+    y=int(input("Type y:"))
+  
+    if desk[x][y] == 1:
+      print("Есть попадание!")
+      desk[x][y]=0
+    
+    else:
+      print("Мимо!")
+      
+  except IndexError:
+    print("Что гонишь что ли? Заново давай!")
 
 
 if __name__=="__main__":
@@ -11,13 +27,19 @@ if __name__=="__main__":
   introduction()
   
   # testing creation of enemy's desk
-  enemy_desk=Desk(11,13)
+  enemy_desk=Desk(3,3)
   enemy_desk.create_enemy()
   
   enemy_desk.print_enemy()
   
   
+  while True:
+    shoot(enemy_desk.enemy_field)
+    if not enemy_desk.check_ships():
+      print("Вы победили!")
+      break
+    enemy_desk.print_enemy()
+
+
   # ships=Ships()
-  
-  
   # print(ships.ships)
